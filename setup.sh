f58e68d71a4f9e4b8d332908e9b6ba441d1f54a0
nic <nwanavit@hatari.cc>
2019-06-02 04:08:21 +0700

    add check for ctags

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 mv ~/.vim ~/.vim.backup
 mv ~/.vimrc ~/.vimrc.backup
 
+# check and install ctags
+if ! dpkg-query -W -f='${Status}' exuberant-ctags | grep "ok installed"; then apt install exuberant-ctags; fi
+
 for _i in $HOME/{".vimbackup",".vimbackup/backups",".vimbackup/swaps"}
 do
 				[ ! -d "$_i" ] && mkdir "$_i"
